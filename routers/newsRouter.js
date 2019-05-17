f16a3d972fc6256fb0e715e8655b930b5cae6869
chenxi <cx.he@outlook.com>
2019-05-17 22:34:11 +0800

    v-0.0.2

--- routers/newsRouter.js
+++ routers/newsRouter.js
@@ -11,49 +11,49 @@ var imgUrlFun = function(str){
 	return data
 }
 
-var connection = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '',
-   database : 'ITnews'
-});
-connection.connect()
+// var connection = mysql.createConnection({
+//    host     : 'localhost',
+//    user     : 'root',
+//    password : '',
+//    database : 'ITnews'
+// });
+// connection.connect()
 
 var router = express.Router()
-router.get('/', (req, res, next) => {
-   if(req.query.page){
-      let  sql = 'SELECT id,abstract,title,cover_img_url,source_logo_url,source_name,date FROM news WHERE id<'+req.query.page+' ORDER BY id DESC LIMIT 0,5'
-      //查
-      connection.query(sql,function (err, result) {
-         if(err){
-         console.log('[SELECT ERROR] - ',err.message);
-         return;
-         }
-         res.send(result)
-      })
-   } else{
-      let  sql = 'SELECT id,abstract,title,cover_img_url,source_logo_url,source_name,date FROM news ORDER BY id DESC LIMIT 0,5'
-      //查
-      connection.query(sql,function (err, result) {
-         if(err){
-         console.log('[SELECT ERROR] - ',err.message);
-         return;
-         }
-         res.send(result)
-      })
-   }
-})
-router.get('/explore', (req, res, next) => {
-   let  sql = 'SELECT * FROM sites ORDER BY id  LIMIT 0,5'
-   //查
-   connection.query(sql,function (err, result) {
-      if(err){
-      console.log('[SELECT ERROR] - ',err.message);
-      return;
-      }
-      res.send(result)
-   })
-})
+// router.get('/', (req, res, next) => {
+//    if(req.query.page){
+//       let  sql = 'SELECT id,abstract,title,cover_img_url,source_logo_url,source_name,date FROM news WHERE id<'+req.query.page+' ORDER BY id DESC LIMIT 0,5'
+//       //查
+//       connection.query(sql,function (err, result) {
+//          if(err){
+//          console.log('[SELECT ERROR] - ',err.message);
+//          return;
+//          }
+//          res.send(result)
+//       })
+//    } else{
+//       let  sql = 'SELECT id,abstract,title,cover_img_url,source_logo_url,source_name,date FROM news ORDER BY id DESC LIMIT 0,5'
+//       //查
+//       connection.query(sql,function (err, result) {
+//          if(err){
+//          console.log('[SELECT ERROR] - ',err.message);
+//          return;
+//          }
+//          res.send(result)
+//       })
+//    }
+// })
+// router.get('/explore', (req, res, next) => {
+//    let  sql = 'SELECT * FROM sites ORDER BY id  LIMIT 0,5'
+//    //查
+//    connection.query(sql,function (err, result) {
+//       if(err){
+//       console.log('[SELECT ERROR] - ',err.message);
+//       return;
+//       }
+//       res.send(result)
+//    })
+// })
 // router.get('/morecomments', (req, res, next) => {
 //    if (req.query.bottom_comment_id&&req.query.newsId){
 //       let  sql = 'SELECT * FROM comments WHERE id<'+req.query.bottom_comment_id+' AND news_id='+req.query.newsId+' ORDER BY id DESC LIMIT 0,5'
@@ -70,40 +70,40 @@ router.get('/explore', (req, res, next) => {
 //    }
 
 // })
-router.get('/article', (req, res, next) => {
-   let data ={}
-   if (req.query.newsId){
-      let  sql = 'SELECT * FROM news WHERE id='+req.query.newsId
-      //查
-      connection.query(sql,function (err, result) {
-         if(err){
-         console.log('[SELECT ERROR] - ',err.message);
-         return;
-         }
-         console.log(result)
-         data.news = result
-         let sql = 'SELECT * FROM comments WHERE news_id='+req.query.newsId+' ORDER BY support_num DESC LIMIT 0,3'
-         connection.query(sql,function (err, result) {
-            if(err){
-               console.log('[SELECT ERROR] - ',err.message);
-               return;
-            }
-            data.hot_comments = result
-            let sql = 'SELECT * FROM comments WHERE news_id='+req.query.newsId+' ORDER BY id DESC LIMIT 0,5'
-            connection.query(sql,function (err, result) {
-               if(err){
-                  console.log('[SELECT ERROR] - ',err.message);
-                  return;
-               }
-               data.all_comments = result
-               res.send(data)
-            })
-         })
-      });
-   } else {
-      res.status(500).send('error')
-   }
-})
+// router.get('/article', (req, res, next) => {
+//    let data ={}
+//    if (req.query.newsId){
+//       let  sql = 'SELECT * FROM news WHERE id='+req.query.newsId
+//       //查
+//       connection.query(sql,function (err, result) {
+//          if(err){
+//          console.log('[SELECT ERROR] - ',err.message);
+//          return;
+//          }
+//          console.log(result)
+//          data.news = result
+//          let sql = 'SELECT * FROM comments WHERE news_id='+req.query.newsId+' ORDER BY support_num DESC LIMIT 0,3'
+//          connection.query(sql,function (err, result) {
+//             if(err){
+//                console.log('[SELECT ERROR] - ',err.message);
+//                return;
+//             }
+//             data.hot_comments = result
+//             let sql = 'SELECT * FROM comments WHERE news_id='+req.query.newsId+' ORDER BY id DESC LIMIT 0,5'
+//             connection.query(sql,function (err, result) {
+//                if(err){
+//                   console.log('[SELECT ERROR] - ',err.message);
+//                   return;
+//                }
+//                data.all_comments = result
+//                res.send(data)
+//             })
+//          })
+//       });
+//    } else {
+//       res.status(500).send('error')
+//    }
+// })
 router.get('/getrssdata',(reQ,reS)=>{
    if (reQ.query.rssurl){
       var req = request(reQ.query.rssurl)
